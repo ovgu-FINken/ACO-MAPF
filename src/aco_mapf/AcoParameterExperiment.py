2e9f3bd19599f5cfdbde2467dcdb59dc3fa65ccc
Sebastian Mai <sebastian.mai@ovgu.de>
2019-06-18 11:12:23 +0200

    adding optimization

--- src/aco_mapf/AcoParameterExperiment.py
+++ src/aco_mapf/AcoParameterExperiment.py
@@ -17,6 +17,10 @@ def run_testprolem_aco(seed=0, num_agents=1, log_steps=20, between_log_steps=50,
     return pd.concat(data)
 
 
+def eval_testproblem_aco(df):
+    return df["max_best_distance"].mean()
+
+
 def run_experiment(name, interactive=False):
     print(f"running experiment for parameters {name}.json, saving to {name}.pkl")
     experiment = Experiment(function=run_testprolem_aco)
@@ -27,6 +31,17 @@ def run_experiment(name, interactive=False):
     experiment.run_map(interactive=True)
     print("saving")
     experiment.save_results(f"{name}.pkl")
+
+def run_optimization(name, generations=100):
+    optimizer = Optimizer(function=run_testprolem_aco, evaluation_function=eval_testproblem_aco, population_size=10)
+    optimizer.load_parameters(f"{name}.json")
+    optimizer.init_population(10)
+    for _ in range(generations):
+        print(f"{optimizer.generation} optimzer.best: {optimizer.global_best_fitness}\n{optimizer.global_best}")
+        optimizer.run_generation()
+    optimizer.save_results(f"{name}.pkl")
+    optimizer.save_parameters(f"{name}.json")
+
 
 if __name__ == "__main__":
     run_experiment(sys.argv[-1])
