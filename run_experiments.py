c1d75c1f6cb89d4577caba763bc72fa65340ebc0
Sebastian Mai <sebastian.mai@ovgu.de>
2019-06-19 12:44:01 +0200

    adding more parameters to test functions

--- run_experiments.py
+++ run_experiments.py
@@ -7,11 +7,18 @@ from src.aco_mapf.AcoParameterExperiment import *
 def optimize(args):
     outfile = args.optimize
     datafile = None
+    eval_kwargs = {}
     if args.data_out:
         datafile = args.data_out
     if args.params_out:
         outfile = args.params_out
-    run_optimization(args.optimize, runs=args.runs, generations=args.generations, outfile=outfile, data_file=datafile, with_cluster=args.parallel)
+    if args.average:
+        eval_kwargs["average"] = args.average
+    if args.property:
+        eval_kwargs["property"] = args.property
+    if args.step:
+        eval_kwargs["step"] = args.step
+    run_optimization(args.optimize, runs=args.runs, generations=args.generations, outfile=outfile, data_file=datafile, with_cluster=args.parallel, population_size=args.population)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -20,8 +27,13 @@ if __name__ == "__main__":
     parser.add_argument("--params_out", help='file name to write back parameters, default is to write back to the input!')
     parser.add_argument("--runs", help="number of runs", default=31, type=int)
     parser.add_argument("--generations", help="number of generations to run", default=20, type=int)
+    parser.add_argument("--population", help="population size", default=10, type=int)
     parser.add_argument("--parallel", action="store_true", default=True)
     parser.add_argument("--sequential", action="store_false", dest="parallel")
+    parser.add_argument("--step", type=int, help="step number of the fitness evaluation")
+    parser.add_argument("--average", help="used as the averaging method: either mean or median")
+    parser.add_argument("--median", help="use median as averaging method in evaluation", action="store_const", const="median", dest="average")
+    parser.add_argument("--property", help="property to use in the evaluation i.e. min_best_distance")
     args = parser.parse_args()
     if args.optimize:
         optimize(args)
--- src/aco_mapf/AcoParameterExperiment.py
+++ src/aco_mapf/AcoParameterExperiment.py
@@ -1,14 +1,26 @@
 from ExperimentRunner.ExperimentRunner import *
-import sys
 import ipyparallel as ipp
 from src.aco_mapf.AcoAgent import AcoAgent, Colony
 from src.aco_mapf.GraphWorld import TestProblem
+import functools
 
 @ipp.require("pandas as pd", "numpy as np", TestProblem, AcoAgent, Colony)
-def run_testprolem_aco(seed=0, num_agents=1, log_steps=20, between_log_steps=50,**kwargs):
+def run_testprolem_aco(seed=0, num_agents=1, log_steps=20, between_log_steps=50, problem="hard_1", **kwargs):
     c = Colony()
     agents = [AcoAgent(seed=seed+offset, colony=c, **kwargs) for offset in range(num_agents)]
-    problem = TestProblem(seed=seed).hard_1(agents=agents)
+    #problem = None
+    if problem == "hard_1":
+        problem = TestProblem(seed=seed).hard_1(agents=agents)
+    elif problem == "hard_2":
+        problem = TestProblem(seed=seed).hard_2(agents=agents)
+    elif problem == "easy_1":
+        problem = TestProblem(seed=seed).easy_1(agents=agents)
+    elif problem == "easy_2":
+        problem = TestProblem(seed=seed).easy_2(agents=agents)
+    elif problem == "easy_3":
+        problem = TestProblem(seed=seed).easy_3(agents=agents)
+    elif problem == "easy_4":
+        problem = TestProblem(seed=seed).easy_4(agents=agents)
     data = []
     for _ in range(log_steps):
         for _ in range(between_log_steps):
@@ -17,34 +29,52 @@ def run_testprolem_aco(seed=0, num_agents=1, log_steps=20, between_log_steps=50,
     return pd.concat(data)
 
 
-def eval_testproblem_aco(df):
+def eval_min_best_distance_mean(df):
     #print(df.keys())
     fitness = df["min_best_distance"].mean()
     return fitness
 
 
-def run_experiment(name, interactive=False):
+def eval_df(data, step=None, avg="mean", property="min_best_distance", verbose=False, **kwargs):
+    if(len(kwargs) > 0):
+        print(f"eval got unrecognized kwargs:\n{kwargs}")
+    df = data
+    if step is not None:
+        if step > 0:
+            df = df.loc[df.world_steps == step]
+        elif step < 0:
+            raise NotImplementedError
+    fitness = np.nan
+    if avg == "median":
+        fitness = df[property].median()
+    fitness = df[property].mean()
+    if verbose:
+        print(f"fitness: {fitness}")
+    return fitness
+
+def run_experiment(name):
     print(f"running experiment for parameters {name}.json, saving to {name}.pkl")
     experiment = Experiment(function=run_testprolem_aco, param_file=f"{name}.json")
     print(f"Got {len(experiment.parameters)} parameters.")
     experiment.generate_tasks()
     print(f"Generating running {len(experiment.tasks)} tasks.")
-    experiment.run_map(interactive=True)
+    experiment.run_map()
     print("saving")
     experiment.save_results(f"{name}.pkl")
 
-def run_optimization(filename, generations=20, runs=31, outfile=None, data_file=None, **kwargs):
+def run_optimization(filename, generations=20, runs=31, outfile=None, data_file=None, eval_kwargs={}, population_size=10, **kwargs):
     optimizer = Optimizer(function=run_testprolem_aco,
-                          evaluation_function=eval_testproblem_aco,
-                          population_size=10,
+                          evaluation_function=functools.partial(eval_df, **eval_kwargs),
+                          population_size=population_size,
                           param_file=filename,
                           runs=runs, **kwargs)
     try:
         for _ in range(generations):
             print(f"{optimizer.generation} optimzer.best: {optimizer.global_best_fitness}\n{optimizer.global_best}")
-            print([str(p) for p in optimizer.mapping])
-            print("\n\n\n")
+            print("\n".join([f"{p.name} -- {p.best} from [{p.low}, {p.high}]" for p in optimizer.mapping]))
             optimizer.run_generation()
+            print(optimizer.fitness)
+            print("\n\n\n")
         if data_file:
             optimizer.save_results(data_file)
         if outfile:
@@ -55,4 +85,4 @@ def run_optimization(filename, generations=20, runs=31, outfile=None, data_file=
 
 
 if __name__ == "__main__":
-    run_optimization("optimize", runs=1, with_cluster=False)
+    run_optimization("optimize.json", runs=1, with_cluster=False)
